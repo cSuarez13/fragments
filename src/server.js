18eba4688e2ed8ca0dc47aae2327188e62dbf531
Claudia Suarez <csuarez-socorro@myseneca.ca>
2025-01-16 22:15:27 -0500

    Switched to src/index.js as main entry point

--- src/server.js
+++ src/server.js
@@ -9,6 +9,13 @@ const logger = require('./logger');
 // Get our express app instance
 const app = require('./app');
 
+// Check if LOG_LEVEL is set to debug
+if (process.env.LOG_LEVEL === 'debug') {
+  // Print the entire process environment (excluding sensitive data if necessary)
+  console.log('=== ENVIRONMENT VARIABLES ===');
+  console.log(process.env); // This prints all environment variables
+}
+
 // Get the desired port from the process' environment. Default to `8080`
 const port = parseInt(process.env.PORT || '8080', 10);
 
